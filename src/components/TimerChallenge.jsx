3e322c71034cfea153a848e9df5e9b524a9427ae
Akhliddin Akhmadjonov <ahliddinahmadjonov1123@gmail.com>
2024-03-21 15:38:12 +0100

    adding Challenges

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,8 +1,14 @@
-export default function TimerChallange({ title }) {
+export default function TimerChallenge({ title, targetTime }) {
   return (
-    <section className="challange">
+    <section className="challenge">
       <h2>{title}</h2>
-      <p className="challange-time"></p>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? "s" : ""}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p className="">Time is running...</p>
     </section>
   );
 }
