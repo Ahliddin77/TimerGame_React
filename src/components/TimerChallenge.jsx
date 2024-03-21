5d1276385661a6508603414023c99d4665c89289
Akhliddin Akhmadjonov <ahliddinahmadjonov1123@gmail.com>
2024-03-21 15:29:19 +0100

    dislplay playerName with ref

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,8 @@
+export default function TimerChallange({ title }) {
+  return (
+    <section className="challange">
+      <h2>{title}</h2>
+      <p className="challange-time"></p>
+    </section>
+  );
+}
